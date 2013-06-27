bd77b680d3a9243cefa2f1dd1e6718143d5145a5
Andrii Koval <a.koval@list.ru>
2013-06-26 20:30:13 -0400

    w

--- web.js
+++ web.js
@@ -1,2 +1,17 @@
-var outfile = "hello.txt";
-var out = "A startup is a business built to grow rapidly.\n";
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+	var fs = require('fs');
+	var outfile = "hello.txt";
+	var out = "Modify this script to write out something different.\n";
+	fs.writeFileSync(outfile, out);  
+	console.log("Script: " + __filename + "\nWrote: " + out + "To: " + outfile);  
+	response.send('Hello World!');
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
