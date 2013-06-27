00b368d0de0e7b02ea74b48a2bc8d1f7fa10851e
Andrii Koval <a.koval@list.ru>
2013-06-26 20:25:49 -0400

    w

--- web.js
+++ web.js
@@ -1,5 +1,6 @@
-var fs = require('fs');
+#!/usr/bin/env node
+//var fs = require('fs');
 var outfile = "hello.txt";
 var out = "A startup is a business built to grow rapidly.\n";
-fs.writeFileSync(outfile, out);  
-console.log("Script: " + __filename + "\nWrote: " + out + "To: " + outfile);
+//fs.writeFileSync(outfile, out);  
+//console.log("Script: " + __filename + "\nWrote: " + out + "To: " + outfile);
