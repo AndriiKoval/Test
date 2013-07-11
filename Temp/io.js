64ca13e8425bd60558d3bf5b132119e5070310b1
Andrii Koval <a.koval@list.ru>
2013-07-11 10:00:35 -0400

    ц

--- Temp/io.js
+++ Temp/io.js
@@ -1,5 +1,12 @@
 var fs = require('fs');
+var r = require('restler');
 
 var f = fs.readFileSync("index.html");
+
+function onSuccess(data, response) {
+	console.log(response.rawEncoded.toString());
+}
+
+r.get("http://obscure-gorge-5935.herokuapp.com").on("success", onSuccess);
 
 console.log("Read: " + f);
